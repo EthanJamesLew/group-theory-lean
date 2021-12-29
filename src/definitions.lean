a1fe794cda811ecf1df0c8317d094f2e38e8c7df
Ethan Lew <elew@galois.com>
2021-12-29 13:09:06 -0800

    add more exercises

--- src/definitions.lean
+++ src/definitions.lean
@@ -12,10 +12,10 @@ variables {A : Type}
 -- (i) a ∈ G → a ⬝ e = a
 -- (ii) a ∈ G -> ∃ a⁻¹ ∈ G, a⁻¹ ⬝ a = e 
 class Group (A : Type) :=
-  (mul : A → A → A)
-  (mul_assoc : ∀ a b c, mul (mul a b) c = mul a (mul b c))
-  (inv : A → A)
   (one : A)
+  (mul : A → A → A)
+  (inv : A → A)
+  (mul_assoc : ∀ a b c, mul (mul a b) c = mul a (mul b c))
   (mul_one : ∀ a, mul a one = a) 
   (mul_inv : ∀ a, mul a (inv a) = one)
 
@@ -41,13 +41,6 @@ calc  one * a = a * a⁻¹ * a : by rw  Group.mul_inv
           ... = a * one : by simp [Group.mul_assoc, inv_mul]
           ... = a : by simp [Group.mul_one]
 
-lemma inv_inv [G : Group A] (a : A) : (a⁻¹)⁻¹ = a :=
-  calc  (a⁻¹)⁻¹ = (a⁻¹)⁻¹ * one : by rw Group.mul_one
-            ... = (a⁻¹)⁻¹ * (a⁻¹ * a) : by rw inv_mul
-            ... = ((a⁻¹)⁻¹ * a⁻¹) * a : by simp [Group.mul_assoc]
-            ... =  one * a : by rw inv_mul
-            ... = a : by rw one_mul
-
 -- this is essentially the proof strategy for Exercise 1.2.3
 lemma prod_to_inv [G : Group A] (a b c : A): a * b = c ↔ b = a⁻¹ * c :=
 begin
@@ -70,5 +63,51 @@ begin
   split,
   rw prod_to_inv,
 end
+
+-- Exercise 1.2.5
+example [G : Group A] (a x : A) : a * x = a ↔ x = one :=
+begin
+  split,
+  intro p,
+  rw prod_to_inv at p,
+  rw inv_mul at p,
+  exact p,
+  intro p,
+  rw prod_to_inv,
+  rw inv_mul,
+  exact p,
+end
+
+-- Exercise 1.2.6
+lemma prod_one_inv [G : Group A] (a b : A) : a * b = one ↔ b = a⁻¹ :=
+begin
+  split,
+  intro p,
+  rw prod_to_inv at p,
+  rw Group.mul_one at p,
+  exact p,
+  intro q,
+  rw q,
+  rw Group.mul_inv,
+end
+
+-- Exercise 1.2.7
+example [G : Group A] (a b : A) : (b⁻¹ * a⁻¹) = (a * b)⁻¹ :=
+begin
+  rw ←prod_one_inv,
+  calc a * b * (b⁻¹ * a⁻¹) = a * (b * b⁻¹) * a⁻¹ : by simp [Group.mul_assoc]
+                      ... = a * one * a⁻¹ : by rw [Group.mul_inv] 
+                      ... = a * a⁻¹ : by rw Group.mul_one
+                      ... = one : by rw Group.mul_inv,
+end 
+
+-- Exercise 1.2.8
+lemma inv_inv [G : Group A] (a : A) : (a⁻¹)⁻¹ = a :=
+  calc  (a⁻¹)⁻¹ = (a⁻¹)⁻¹ * one : by rw Group.mul_one
+            ... = (a⁻¹)⁻¹ * (a⁻¹ * a) : by rw inv_mul
+            ... = ((a⁻¹)⁻¹ * a⁻¹) * a : by simp [Group.mul_assoc]
+            ... =  one * a : by rw inv_mul
+            ... = a : by rw one_mul
+
 
 end definitions
